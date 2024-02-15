6e4671c057882d596cfcf4fcf460d76a3d2663ba
CoryFluker <160174429+CoryFluker@users.noreply.github.com>
2024-02-15 14:49:24 -0500

    Add files via upload

---
+++ P1LAB1_FlukerCorrion.py
@@ -0,0 +1,13 @@
+# Corrion Fluker
+# 2-5-2024
+# PLAB1
+# The program will display my name in a welcome message
+
+# get the first name from the user assign it to a variable
+
+fname =input("Enter your first name: ")
+#print(fname)
+lname =input("Enter your last name: ")
+print(lname)
+#print out the names in a welcome message
+print("Hello,",fname,lname +'!Welcome to CTI-110')
